20961856a05558299aa8556d2d719dfec5f9f409
Martin <sanchez.martin803@gmail.com>
2023-04-30 15:12:46 -0500

    delete comment block of unnedded code

--- frontend/src/components/service.vue
+++ frontend/src/components/service.vue
@@ -115,24 +115,6 @@ export default {
               <option value="Inactive">Inactive</option>
             </select>
             </div>
-            <!--<label class="block">
-              <span class="text-gray-700">Service Status</span>
-              <input
-                type="dropdown"
-                class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                placeholder
-                v-model="service.status"
-              />
-              <span class="text-black" v-if="v$.service.status.$error">
-                <p
-                  class="text-red-700"
-                  v-for="error of v$.service.status.$errors"
-                  :key="error.$uid"
-                >
-                  {{ error.$message }}!
-                </p>
-              </span>
-            </label> -->
           </div>
           <!-- submit button -->
           <div class="flex justify-between mt-10 mr-20">
